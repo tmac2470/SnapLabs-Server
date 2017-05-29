6742a8b1aa7e2e1489487228ba9a5ec02bc16c4e
MushrChun <mushrchun@gmail.com>
2017-05-29 14:45:42 +1000

    add upload and get one experiment interface

--- controller/experimentController.js
+++ controller/experimentController.js
@@ -1,8 +1,8 @@
 /**
  * Created by MushrChun on 22/5/17.
  */
-const Experiment = require('../model/Experiment');
-const ObjectId = require('mongodb').ObjectId;
+var Experiment = require('../model/Experiment');
+var ObjectId = require('mongodb').ObjectId;
 
 exports.getAllExperiments = (req, res) => {
     Experiment.find({}, 'labTitle').exec((err, experiments) => {
@@ -10,5 +10,29 @@ exports.getAllExperiments = (req, res) => {
             return res.send(err);
         }
         res.json(experiments);
+    });
+};
+
+exports.getOneExperiment = (req, res) => {
+    var title = req.params.title;
+    console.log(title)
+    Experiment.findOne({labTitle: title}).exec((err, experiments) => {
+        if (err) {
+            return res.send(err);
+        }
+        res.json(experiments);
+    });
+};
+
+exports.putOneExperiment = (req, res) => {
+    var content = req.body;
+    var newExp = new Experiment(content);
+    newExp.save(function(err, result){
+        if(err){
+            console.log(err);
+            res.json({status: 'fail'});
+        }else{
+            res.json({status: 'success'});
+        }
     });
 };
--- routes/experiment.js
+++ routes/experiment.js
@@ -5,4 +5,8 @@ var controller = require('../controller/experimentController');
 /* GET users listing. */
 router.get('/', controller.getAllExperiments);
 
+router.get('/getOne/:title', controller.getOneExperiment);
+
+router.post('/putOne', controller.putOneExperiment);
+
 module.exports = router;
